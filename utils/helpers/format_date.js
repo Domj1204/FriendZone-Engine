c6f7d99d5536dd624e2f6f7b2f832e104ccfbe1e
Domj1204 <148828606+Domj1204@users.noreply.github.com>
2024-04-01 20:26:11 -0500

    finished format_date.js

--- utils/helpers/format_date.js
+++ utils/helpers/format_date.js
@@ -21,3 +21,36 @@ function format_date(timestamp) {
 				
 	return `${month} ${day}${suffix}, ${year} at ${time}`
 }
+
+/**
+ * @function getDaySuffix
+ * Returns the suffix to add after day based on its value
+ * @param {*} day 
+ * @returns {String} suffix
+ */
+function getDaySuffix(day) {
+	var suffix; 
+
+	if (day > 10 && day < 20) {
+		// Return the suffix 'th' if the day is 11-19
+		suffix = 'th';
+	} else { 
+		// Read the number in the oneth place and update suffix based on the value
+		switch(day % 10) {
+			case 1:
+				suffix = 'st';
+				break;
+			case 2:
+				suffix = 'nd';
+				break;
+			case 3:
+				suffix = 'rd';
+				break;
+			default: 
+			suffix = 'th';
+		}
+	}
+	return suffix;
+}
+
+module.exports = format_date;
